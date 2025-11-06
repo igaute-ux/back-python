caef3e675b88fc8b06cfd440ad8dad70b55d516d
IgnacioGaute <nachitogaute@gmail.com>
2025-11-06 14:24:26 -0300

    fix: route

--- app/api/router.py
+++ app/api/router.py
@@ -1,6 +1,6 @@
+# app/api/router.py
 from fastapi import APIRouter
 from app.api.routes import esg
 
 api_router = APIRouter()
-
-api_router.include_router(esg.router, prefix="/esg", tags=["esg"])
+api_router.include_router(esg.router, prefix="", tags=["esg"])
